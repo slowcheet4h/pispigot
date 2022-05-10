fcd5515878822e3e7fbb5f1fcbcee7c5b546be61
deadghost <69243988+deadghost2173@users.noreply.github.com>
2022-05-10 18:58:33 +0300

    one event system :D

--- pisi/unitedmeows/pispigot/Pispigot.java
+++ pisi/unitedmeows/pispigot/Pispigot.java
@@ -1,42 +1,26 @@
 package pisi.unitedmeows.pispigot;
 
 import org.bukkit.Bukkit;
-import org.bukkit.entity.Player;
 import org.bukkit.plugin.java.JavaPlugin;
-import pisi.unitedmeows.eventapi.event.Event;
+
 import pisi.unitedmeows.eventapi.system.BasicEventSystem;
 import pisi.unitedmeows.pispigot.rawlistener.PRawListener;
 
-import java.util.HashMap;
-
 public class Pispigot extends JavaPlugin {
-
-
-	private static HashMap<Player, BasicEventSystem> _EVENT_SYSTEMS;
 	private static Pispigot INSTANCE;
-
-	static {
-		_EVENT_SYSTEMS = new HashMap<>();
-	}
+	private BasicEventSystem eventSystem;
 
 	@Override
 	public void onEnable() {
 		INSTANCE = this;
-		_EVENT_SYSTEMS.entrySet().removeIf(k -> !k.getKey().isOnline());
 		Bukkit.getServer().getPluginManager().registerEvents(new PRawListener(), INSTANCE);
-		for (Player player : Bukkit.getOnlinePlayers()) {
-			_EVENT_SYSTEMS.computeIfAbsent(player, k -> new BasicEventSystem());
-		}
 	}
 
-
-	public static BasicEventSystem removeSystem(Player player) {
-		return _EVENT_SYSTEMS.remove(player);
+	public BasicEventSystem eventSystem() {
+		return eventSystem;
 	}
 
-	public static BasicEventSystem eventSystem(Player player) {
-		return _EVENT_SYSTEMS.computeIfAbsent(player, k -> new BasicEventSystem());
+	public static Pispigot get() {
+		return INSTANCE;
 	}
-
-
 }
--- pisi/unitedmeows/pispigot/rawlistener/PRawListener.java
+++ pisi/unitedmeows/pispigot/rawlistener/PRawListener.java
@@ -5,7 +5,10 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
-import org.bukkit.event.player.*;
+import org.bukkit.event.player.PlayerBedEnterEvent;
+import org.bukkit.event.player.PlayerBedLeaveEvent;
+import org.bukkit.event.player.PlayerMoveEvent;
+
 import pisi.unitedmeows.pispigot.Pispigot;
 import pisi.unitedmeows.pispigot.events.player.EventPlayerAttack;
 import pisi.unitedmeows.pispigot.events.player.EventPlayerBed;
@@ -13,37 +16,24 @@ import pisi.unitedmeows.pispigot.events.player.EventPlayerDamagedByEntity;
 import pisi.unitedmeows.pispigot.events.player.EventPlayerMotion;
 
 public class PRawListener implements Listener {
-
-
-	@EventHandler
-	public void onJoin(PlayerJoinEvent event) {
-		Pispigot.eventSystem(event.getPlayer());
-	}
-
-	@EventHandler
-	public void onLeave(PlayerQuitEvent event) {
-		Pispigot.removeSystem(event.getPlayer());
-	}
-
 	@EventHandler
 	public void onBedEnter(PlayerBedEnterEvent event) {
 		final EventPlayerBed pisiEvent = new EventPlayerBed(event.getPlayer(), event.getBed(), EventPlayerBed.BedAction.SLEEP);
 		pisiEvent.setCanceled(event.isCancelled());
-		Pispigot.eventSystem(event.getPlayer()).fire(pisiEvent);
+		Pispigot.get().eventSystem().fire(pisiEvent);
 		event.setCancelled(pisiEvent.isCanceled());
 	}
 
 	@EventHandler
 	public void onBedLeave(PlayerBedLeaveEvent event) {
-		Pispigot.eventSystem(event.getPlayer()).fire(new EventPlayerBed(event.getPlayer(), event.getBed(), EventPlayerBed.BedAction.LEAVE));
+		Pispigot.get().eventSystem().fire(new EventPlayerBed(event.getPlayer(), event.getBed(), EventPlayerBed.BedAction.LEAVE));
 	}
-
 
 	@EventHandler
 	public void onMove(PlayerMoveEvent event) {
 		final EventPlayerMotion pisiEvent = new EventPlayerMotion(event.getPlayer(), event.getFrom(), event.getTo());
 		pisiEvent.setCanceled(event.isCancelled());
-		Pispigot.eventSystem(event.getPlayer()).fire(pisiEvent);
+		Pispigot.get().eventSystem().fire(pisiEvent);
 		event.setCancelled(pisiEvent.isCanceled());
 	}
 
@@ -51,25 +41,19 @@ public class PRawListener implements Listener {
 	public void onAttack(EntityDamageByEntityEvent event) {
 		final Entity attacker = event.getDamager();
 		final Entity damaged = event.getEntity();
-
 		if (attacker instanceof Player) {
 			EventPlayerAttack pisiEvent = new EventPlayerAttack((Player) attacker, damaged, event.getEntityType(), event.getDamage(), event.getFinalDamage());
 			pisiEvent.setCanceled(event.isCancelled());
-			Pispigot.eventSystem((Player) attacker).fire(pisiEvent);
+			Pispigot.get().eventSystem().fire(pisiEvent);
 			event.setCancelled(pisiEvent.isCanceled());
 			event.setDamage(pisiEvent.damage());
 		}
-
-
 		if (damaged instanceof Player) {
 			EventPlayerDamagedByEntity pisiEvent = new EventPlayerDamagedByEntity((Player) damaged, attacker, event.getDamage(), event.getFinalDamage());
 			pisiEvent.setCanceled(event.isCancelled());
-			Pispigot.eventSystem((Player) damaged).fire(pisiEvent);
+			Pispigot.get().eventSystem().fire(pisiEvent);
 			event.setCancelled(pisiEvent.isCanceled());
 			event.setDamage(pisiEvent.damage());
 		}
 	}
-
-
-
 }
